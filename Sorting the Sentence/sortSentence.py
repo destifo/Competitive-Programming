119083a05042b11abbd11defac4236c01d0525d5
destifo <estifanosbireda@gmail.com>
2022-02-06 12:47:49 +0300

    added solution to sort sentence leetcode

---
+++ Sorting the Sentence/sortSentence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        str = s.split()
+        str_lst = [0] * 10
+        for word in str:
+            str_lst[int(word[-1])] = word[:-1] + " "
+        final_str = ""
+        for word in str_lst:
+            if (word != 0):
+                final_str += word
+        return final_str.rstrip()
+
+sol = Solution()
+sol.sortSentence("is2 sentence4 This1 a3")
