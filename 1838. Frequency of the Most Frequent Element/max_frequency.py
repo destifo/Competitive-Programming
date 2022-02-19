1efd307caa072f4945b363888f7beb038cabeef4
destifo <estifanosbireda@gmail.com>
2022-02-19 17:03:55 +0300

    added solution to maxFrequency --leetcode

---
+++ 1838. Frequency of the Most Frequent Element/max_frequency.py
@@ -0,0 +1,25 @@
+'''
+https://leetcode.com/problems/frequency-of-the-most-frequent-element/submissions/
+'''
+
+
+class Solution:
+    def maxFrequency(self, nums, k: int) -> int:
+        nums.sort()
+        left_end, right_end = 0, 0
+        total, max_freq = 0, 0
+
+        while right_end < len(nums):
+            total += nums[right_end]
+
+            while (right_end - left_end + 1)*nums[right_end] > total + k:
+                total -= nums[left_end]
+                left_end += 1
+
+            max_freq = max(max_freq, (right_end - left_end + 1))
+            right_end += 1
+
+        return max_freq
+
+sol = Solution()
+print(sol.maxFrequency([3,9,6], 2))
