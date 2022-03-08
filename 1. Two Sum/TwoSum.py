917dd02016bf1b46626d1f6339cb6938e0ef1157
destifo <estifanosbireda@gmail.com>
2022-03-08 22:38:00 +0300

    added solution to twoSum 1 --leetcode

---
+++ 1. Two Sum/TwoSum.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+class Solution:
+    def twoSum(self, nums, target: int):
+        n = len(nums)
+        nums_dict = dict()
+        for i in range(n):
+            nums_dict[nums[i]] = i
+        
+        for i in range(n):
+            value = target - nums[i]
+            if value in nums_dict.keys() and nums_dict[value] != i:
+                result = [i, nums_dict[value]]
+                return result
+
+
+sol = Solution()
+print(sol.twoSum([2,5,5,11],10))
