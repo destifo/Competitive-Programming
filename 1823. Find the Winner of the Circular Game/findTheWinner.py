494a19312aa4e63823ff31b300dda5af05588fdb
destifo <estifanosbireda@gmail.com>
2022-03-29 11:52:35 +0300

    added solution to findthewinner of the cicular game --leetcode 1823

---
+++ 1823. Find the Winner of the Circular Game/findTheWinner.py
@@ -0,0 +1,20 @@
+class Solution:
+    # well, worked the first time
+    def findTheWinner(self, n: int, k: int):
+        ppl = []
+        for i in range(1, n + 1):
+            ppl.append(i)
+        if n == 1:
+            return 1
+        game_round = 0
+        cur_index = 0
+        while len(ppl) > 1:
+            game_round +=1
+            cur_index = (cur_index + k - 1)%len(ppl)
+            print(ppl.pop(cur_index))
+
+        return ppl.pop()
+
+
+sol = Solution()
+print(sol.findTheWinner(5, 2))
