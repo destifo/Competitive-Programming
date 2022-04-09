c473287e04d6f00a1831229846a064584c5d8a42
destifo <estifanosbireda@gmail.com>
2022-04-09 22:22:20 +0300

    added solution to middle node --leetcode 876

---
+++ 876. Middle of the Linked List/MiddleNode.java
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+ */
+
+
+public class MiddleNode {
+    // 100% faster leetcode says...well it was nothing to get hyped...probably everyone has provided the same solution...may be, but it says 0ms another scenario is the time is too small for whn the algorithm tried to round it and it ended up being zero
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode () {}
+        ListNode (int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static ListNode middleNode(ListNode head) {
+        int listLength = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            curr = curr.next;
+            listLength++;
+        }
+
+        int mid = listLength/2 + 1;
+        ListNode result = head;
+        for (int i = 0; i < mid; i++)
+            result = resultext;
+            
+        return result;
+    }
+    
+}
