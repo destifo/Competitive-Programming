76684195f4802f29016037835da7201ab247a884
destifo <estifanosbireda@gmail.com>
2022-04-28 00:01:50 +0300

    added slightly time efficent solution for boats to save people --leetcode 881

--- 881. Boats to Save People/NumRescueBoats.py
+++ 881. Boats to Save People/NumRescueBoats.py
@@ -1,4 +1,5 @@
 class Solution:
+    # very space efficent
     def numRescueBoats(self, people, limit: int):
         if len(people) == 0:
             return 0
@@ -23,6 +24,36 @@ class Solution:
 
          
         total_boats += len(people)
+        return total_boats
+    
+    # time efficent(kinda)
+    def numRescueBoats2(self, people, limit) -> int:
+        if len(people) == 0:
+            return 0
+        people.sort()
+        total_boats = 0
+        j = len(people) - 1
+        i = 0
+        while i <= j:
+            if i == j:
+                total_boats +=1
+                return total_boats
+            while people[i] + people[j] > limit:
+                if j < i:
+                    break
+                j -=1
+                total_boats +=1
+
+            if i == j:
+                total_boats +=1
+                return total_boats
+            if i >= j:
+                break
+
+            total_boats +=1
+            i += 1
+            j -= 1
+         
         return total_boats
 
 sol = Solution()
