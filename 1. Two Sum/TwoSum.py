49661f10431d8748844eccf9c02ad55c303dffd5
destifo <estifanosbireda@gmail.com>
2022-05-01 23:56:04 +0300

    added optimal solution to two sum, minor change with slightly better improvement --leetcode 1

--- 1. Two Sum/TwoSum.py
+++ 1. Two Sum/TwoSum.py
@@ -14,6 +14,17 @@ class Solution:
                 result = [i, nums_dict[value]]
                 return result
 
+    def twoSum2(self, nums, target: int):
+        # optimal
+        n = len(nums)
+        nums_dict = dict()
+        for i in range(n):
+            num = nums[i]
+            value = target - num
+            if value in nums_dict.keys():
+                return ([nums_dict[value], i])
+            nums_dict[nums[i]] = i
+
 
 sol = Solution()
 print(sol.twoSum([2,5,5,11],10))
