d7c506d8a295b74cc5be63e57ca03f5ed1c83bf1
destifo <estifanosbireda@gmail.com>
2022-05-02 13:16:54 +0300

    added a more efficent solution to fruit into baskets --leetcode 904

--- 904. Fruit Into Baskets/TotalFruit.py
+++ 904. Fruit Into Baskets/TotalFruit.py
@@ -27,6 +27,33 @@ class Solution:
 
         return max_num_fruits
 
+    def totalFruit2(self, fruits):
+        # checked this one from Nick White @YT, efficent in space and time
+        last_fruit = -1
+        second_last_fruit = -1
+        last_fruit_count = 0
+        current_max = 0
+        max_fruit = 0
+
+        for fruit in fruits:
+            if fruit == last_fruit or fruit == second_last_fruit:
+                current_max +=1
+            else:
+                current_max = last_fruit_count + 1
+
+            if fruit == last_fruit:
+                last_fruit_count +=1
+            else:
+                last_fruit_count = 1
+
+            if fruit != last_fruit:
+                second_last_fruit = last_fruit
+                last_fruit = fruit
+
+            max_fruit = max(max_fruit, current_max)
+
+        return max_fruit
+
 
 sol = Solution()
-print(sol.totalFruit([1,2,1]))
+print(sol.totalFruit2([1,2,3,2,2]))
