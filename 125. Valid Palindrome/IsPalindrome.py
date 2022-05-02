3dc0b737bf84e50c6785da34592fc7c554185380
destifo <estifanosbireda@gmail.com>
2022-05-02 16:10:25 +0300

    added solution for isPalindrome --leetcode 125

---
+++ 125. Valid Palindrome/IsPalindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str):
+        s = s.lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not s[r].isalnum():
+                r -=1
+                continue
+            if not s[l].isalnum():
+                l +=1
+                continue
+            if s[l] != s[r]:
+                return False
+            l +=1
+            r -=1
+
+        return True
+
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
