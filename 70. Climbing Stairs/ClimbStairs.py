31e5553ba2235def24f1450c216452a15cd3da82
destifo <estifanosbireda@gmail.com>
2022-05-10 14:13:28 +0300

    added a naice and efficent solution for Climb Stairs --leetcode 70

---
+++ 70. Climbing Stairs/ClimbStairs.py
@@ -0,0 +1,38 @@
+'''
+https://leetcode.com/problems/climbing-stairs/
+'''
+
+class Solution:
+    def climbStairs(self, n: int):
+        # naive solution+
+        def findNumOfStrairs(rem, tot):
+            if tot == n:
+                return 1
+            
+            if tot > n:
+                return 0
+
+            return findNumOfStrairs(rem - 1, tot + 1) + findNumOfStrairs(rem - 2, tot + 2)
+
+        return findNumOfStrairs(n, 0)
+
+    
+    def climbStairs2(self, n: int):
+        # efficent algorithm using memoization
+        memo = dict()
+        def findNumOfStairs(rem, tot):
+            if tot == n:
+                return 1
+
+            if tot > n:
+                return 0
+
+            if not memo[rem - 1]:
+                memo[rem - 1] = findNumOfStairs(rem - 1, tot - 1)
+
+            if not memo[rem - 2]:
+                memo[rem - 2] = findNumOfStairs(rem - 2, tot - 2)
+
+            return memo[rem - 1] + memo[rem - 2]
+
+        return findNumOfStairs(n, 0)
