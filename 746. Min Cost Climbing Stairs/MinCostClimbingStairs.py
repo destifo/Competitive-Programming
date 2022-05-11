8ea5313011a41e4b037d4d2f74ae8cec0a815cbd
destifo <estifanosbireda@gmail.com>
2022-05-11 22:35:50 +0300

    added an efficent and a naive solution for Min Cost Climbing Stairs --leetcode 746

---
+++ 746. Min Cost Climbing Stairs/MinCostClimbingStairs.py
@@ -0,0 +1,46 @@
+class Solution:
+    # naive approach
+    def minCostClimbingStairs(self, cost):
+        n = len(cost)
+        minCostAt = [-1] * n
+
+
+        def findminCost(stair, currCost):
+            if stair >= n:
+                return currCost
+
+            if stair == n - 1:
+                currCost +=cost[stair]
+                return currCost
+
+            currCost += cost[stair]
+            return min(findminCost(stair + 1, currCost), findminCost(stair + 2, currCost))
+
+        return min(findminCost(0, 0), findminCost(1, 0))
+
+    
+    def minCostClimbingStairs2(self, cost):
+        # very efficent approach using iterative solution 
+        n = len(cost)
+        minCostAt = [-1] * n
+        minCostAt[n - 1] = cost[n - 1]
+        minCostAt[n - 2] = cost[n - 2]
+
+        i = n - 3
+        while i > -1:
+            minCostAt[i] = cost[i] + min(minCostAt[i + 1], minCostAt[i + 2])
+            i -=1
+
+        return min(minCostAt[0], minCostAt[1])
+
+    
+    def minCostClimbingStairs3(self, cost):
+        # very efficent approach by updating the cost list 
+        n = len(cost)
+
+        i = n - 3
+        while i > -1:
+            cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
+            i -=1
+
+        return min(cost[0], cost[1])
