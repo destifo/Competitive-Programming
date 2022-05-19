d8ad16e04d87ec23451f50fcd7ade525e0c8d4b3
destifo <estifanosbireda@gmail.com>
2022-05-19 16:29:37 +0300

    added solution for House Robber --leetcode 198

---
+++ 198. House Robber/Rob.py
@@ -0,0 +1,17 @@
+'''
+https://leetcode.com/problems/house-robber/
+'''
+
+class Solution:
+    # done it 5 mins
+    def rob(self, nums):
+        n = len(nums)
+        dp = [0] * n
+        dp[n - 1] = nums[n - 1]
+        dp[n - 2] = max(dp[n - 1], nums[n - 2])
+        
+        for i in range(n - 3, -1, -1):
+            currVal = dp[i + 2] + nums[i]
+            dp[i] = max(currVal, dp[i + 1])
+
+        return dp[0]
