dd63cdffd53f97fede5d028edcb7092c720bff0c
destifo <estifanosbireda@gmail.com>
2022-06-11 13:53:31 +0300

    added 3 solutions for Minimum Operations to Reduce X to Zero --leetcode 1658

---
+++ 1658. Minimum Operations to Reduce X to Zero/MinOperations.py
@@ -0,0 +1,90 @@
+'''
+https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+'''
+
+
+class Solution:
+
+    # an O(2^n) recursive solution 
+    def minOperations(self, nums: list[int], x: int):
+        def dfs(rem, curr_len, l, r):
+            if rem == 0:
+                return curr_len
+            
+            if rem < 0:
+                return float('inf')
+            
+            chose_left = dfs(rem - nums[l], curr_len + 1, l + 1, r)
+            chose_right = dfs(rem-nums[r], curr_len + 1, l, r - 1)
+            return min(chose_left, chose_right)
+        
+        ans = dfs(x, 0, 0, len(nums) - 1)
+        return -1 if ans == float('inf') else ans
+                
+    
+    # using a memoization, slightly better but still gives TLE
+    def minOperations2(self, nums: list[int], x: int):
+        memo = {}
+        def dfs(rem, curr_len, l, r):
+            if rem == 0:
+                return curr_len
+            
+            if rem < 0:
+                return float('inf')
+            
+            key = str(l) + '#' + str(r) + '#' + str(curr_len)
+            if key in memo.keys():
+                return memo[key]
+
+            chose_left = dfs(rem - nums[l], curr_len + 1, l + 1, r)
+            chose_right = dfs(rem-nums[r], curr_len + 1, l, r - 1)
+            memo[key] = min(chose_left, chose_right)
+            return memo[key]
+        
+        ans = dfs(x, 0, 0, len(nums) - 1)
+        return -1 if ans == float('inf') else ans
+
+
+    # an O(n) solution using two pointer, it really made me sweat to implement,
+    # with the edge cases and stuff..., and the reverse thinking to find the 
+    # the longest subarray with the sum(nums) - x, which helps u to find the min subarray with the sum x in reverse...crazy stuff :-}
+    def minOperations3(self, nums: list[int], x: int):
+        n = len(nums)
+        if n == 1:
+            return 1 if nums[0] == x else -1
+
+        prefix_sum = [0] * (n+1)
+
+        tot = 0
+        for i in range(n):
+            tot +=nums[i]
+            prefix_sum[i + 1] = tot
+
+        nums_sum = prefix_sum[n] - x
+        if nums_sum < 0:    return -1
+        if nums_sum == 0:   return n
+        tot = nums[0]
+        max_len = 0
+        l, r = 0, 1 
+        while r < n:
+            if tot == nums_sum:
+                max_len = max(max_len, r - l)
+            if tot >= nums_sum:
+                tot -=nums[l]
+                l +=1
+                continue
+            
+            num = nums[r]
+            tot +=num
+            while tot > nums_sum:
+                tot -=nums[l]
+                l +=1 
+            r +=1
+        if tot == nums_sum:
+            max_len = max(max_len, r - l)
+
+        return n - max_len if max_len else -1
+            
+
+sol = Solution()
+print(sol.minOperations3([5,1,4,2,3], 6))
