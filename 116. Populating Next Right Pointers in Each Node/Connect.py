6f0f091ff59fb0c99282b1c47ef7a30645799a62
destifo <estifanosbireda@gmail.com>
2022-07-01 14:31:59 +0300

    added another solution for  Populating Next Right Pointers in Each Node --leetcode 116

--- 116. Populating Next Right Pointers in Each Node/Connect.py
+++ 116. Populating Next Right Pointers in Each Node/Connect.py
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 '''
 
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
@@ -10,7 +10,7 @@ class Node:
         self.left = left
         self.right = right
         self.next = next
-"""
+
 
 from collections import deque
 
@@ -40,4 +40,26 @@ class Solution:
                     trvrsd.append(lastNodeOfDepth.right)
 
         bfs(root)
+        return root
+
+    
+    # a simple, problem based solution done on a different occassion
+    def connect2(self, root: 'Node') -> 'Node':
+        qu = deque()
+        qu.append(root)
+        if not root:
+            return root
+        
+        def bfs():
+            while qu:
+                n = len(qu)
+                for i in range(n):
+                    nod = qu.popleft()
+                    if nod.left:
+                        qu.append(nod.left)
+                        qu.append(nod.right)
+                    if i < n-1:
+                        nod.next = qu[0]
+                        
+        bfs()
         return root
