a97fcde0982908c9c505eea8d4f05f5fdea0e5a4
destifo <estifanosbireda@gmail.com>
2022-07-07 16:45:46 +0300

    added a slightly different solution for House Robber --leetcode 198

--- 198. House Robber/Rob.py
+++ 198. House Robber/Rob.py
@@ -15,3 +15,21 @@ class Solution:
             dp[i] = max(currVal, dp[i + 1])
 
         return dp[0]
+
+    
+    # done in some other day
+    def rob2(self, nums: list[int]) -> int:
+        n = len(nums)
+        dp = [0 for i in range(n)]
+        if n == 1:  return nums[0]
+        
+        for i in range(n-1, -1, -1):
+            if i > n - 3:
+                dp[i] = nums[i]
+            elif i == n - 3:
+                dp[i] = nums[i] + nums[i+2]
+            else:
+                dp[i] = nums[i] + max(dp[i+2], dp[i+3])
+            
+            
+        return max(dp[0], dp[1])
