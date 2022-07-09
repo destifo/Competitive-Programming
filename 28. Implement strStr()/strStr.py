3f4d700b559350f3d5f745719f0393adba51bb20
destifo <estifanosbireda@gmail.com>
2022-07-09 16:37:23 +0300

    added a solution for Implement strStr() --leetcode 28

---
+++ 28. Implement strStr()/strStr.py
@@ -0,0 +1,20 @@
+'''
+https://leetcode.com/problems/implement-strstr/
+'''
+
+
+class Solution:
+    # O(n) time complexity as we are going through the string only one time
+    # O(1) space, we just used 4 variables to store some data(pointer and int)
+    def strStr(self, haystack: str, needle: str) -> int:
+        n = len(needle)
+        m = len(haystack)
+        
+        l, r = 0, n
+        while r <= m:
+            if haystack[l:r] == needle:
+                return l
+            l +=1
+            r +=1
+        
+        return -1
