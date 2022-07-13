9f4e502e732f5f196e1361e1e6adccca49fed39e
destifo <estifanosbireda@gmail.com>
2022-07-13 16:19:28 +0300

    added an O(n) time solution for Sum of Subarray Ranges --leetcode 2104

--- 2104. Sum of Subarray Ranges/SubArrayRanges.py
+++ 2104. Sum of Subarray Ranges/SubArrayRanges.py
@@ -19,3 +19,69 @@ class Solution:
                 ans +=(maxm-minm)
                 
         return ans
+
+
+    # The intution behind this solution is genius
+    # O(n) time
+    # O(n) space
+    def subArrayRanges2(self, nums: list[int]) -> int:
+        n = len(nums)
+        tot = 0
+
+        inc_stack = []
+        dec_stack = []
+
+        def pushToIncStack(index:int, stack:list) -> int:
+            tot = 0
+            num = nums[index]
+            while stack and nums[stack[-1]] > num:
+                curr_i = stack.pop()
+                prev_i = stack[-1] if stack else -1
+                tot += (curr_i - prev_i) * (index - curr_i) * nums[curr_i]
+
+            stack.append(i)
+
+            return tot
+
+        def pushToDecStack(index:int, stack:list) -> int:
+            tot = 0
+            num = nums[index]
+            while stack and nums[stack[-1]] < num:
+                curr_i = stack.pop()
+                prev_i = stack[-1] if stack else -1
+                tot += (curr_i - prev_i) * (index - curr_i) * nums[curr_i]
+
+            stack.append(i)
+
+            return tot
+
+        def popAll(index:int) -> int:
+            min_sum = 0
+            max_sum = 0
+
+            stack = dec_stack
+            while stack:
+                curr_i = stack.pop()
+                prev_i = stack[-1] if stack else -1
+                max_sum += (curr_i - prev_i) * (index - curr_i) * nums[curr_i]
+
+            stack = inc_stack
+            while stack:
+                curr_i = stack.pop()
+                prev_i = stack[-1] if stack else -1
+                min_sum += (curr_i - prev_i) * (index - curr_i) * nums[curr_i]
+
+            return max_sum - min_sum
+
+        for i in range(n):
+            sub_min_sum = pushToIncStack(i, inc_stack)
+            sub_max_sum = pushToDecStack(i, dec_stack)
+            tot +=(sub_max_sum - sub_min_sum)
+
+        tot +=popAll(n)
+
+        return tot
+
+
+sol = Solution()
+print(sol.subArrayRanges2([4,-2,-3,4,1]))
