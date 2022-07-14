92f1ab5f5d2428d61ed84a085a6746c36a9e5bcd
destifo <estifanosbireda@gmail.com>
2022-07-14 13:54:38 +0300

    added a solution for Consecutive Characters --leetcode 1446

---
+++ 1446. Consecutive Characters/MaxPower.py
@@ -0,0 +1,29 @@
+'''
+https://leetcode.com/problems/consecutive-characters/
+'''
+
+
+class Solution:
+    # O(n) time,
+    # O(1) space,
+    def maxPower(self, s: str) -> int:
+        n = len(s)
+        power = 0
+        
+        l, r = 0, 0
+        curr_pow = 0
+        while r < n:
+            if s[l] == s[r]:
+                curr_pow +=1
+            else:
+                l = r
+                curr_pow = 1
+            
+            power = max(curr_pow, power)
+            r +=1
+            
+        return power
+
+
+sol = Solution()
+print(sol.maxPower("ccbccbb"))
