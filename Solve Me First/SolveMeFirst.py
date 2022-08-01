f61c56dc8e7378ecbe0745f663e6a96bc468de74
destifo <estifanosbireda@gmail.com>
2022-08-01 21:47:47 +0300

    added a solution for Solve Me First --hackerrank

---
+++ Solve Me First/SolveMeFirst.py
@@ -0,0 +1,16 @@
+'''
+https://www.hackerrank.com/challenges/solve-me-first/problem
+'''
+
+
+# O(1) time,
+# O(1) space, bot lobby problem
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
