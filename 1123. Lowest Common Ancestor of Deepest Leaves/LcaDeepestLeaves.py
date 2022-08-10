cda8d245df53088a548fa70e74412107b58be1fc
destifo <estifanosbireda@gmail.com>
2022-08-11 00:20:38 +0300

    added a solution for Lowest Common Ancestor of Deepest Leaves --leetcode 1123

---
+++ 1123. Lowest Common Ancestor of Deepest Leaves/LcaDeepestLeaves.py
@@ -0,0 +1,58 @@
+'''
+https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+'''
+
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    # O(n) time,
+	# O(1) space, 
+	# Approach: DFS, recursion
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        depth = [0]
+        ancestor = [root]
+        
+        def findDepth(root, length):
+            if root == None:
+                return
+            depth[0] = max(depth[0], length)
+            findDepth(root.left, length+1)
+            findDepth(root.right, length+1)
+            
+        
+        def isDeepLeaf(node, l):
+            return not node.left and not node.right and l == depth[0]
+        
+        def dfs(root, length):
+            if isDeepLeaf(root, length):
+                ancestor[0] = root
+                return True
+            
+            left = False
+            right = False
+            if root.left:
+                left = dfs(root.left, length+1)
+            if root.right:
+                right = dfs(root.right, length+1)
+            
+            if left and right:
+                ancestor[0] = root
+                return True
+            if left or right:
+                return True
+            return False
+        
+        
+        findDepth(root, 0)
+        dfs(root, 0)
+            
+        return ancestor[0]
