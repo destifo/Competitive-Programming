2c9a33dcb266a2d0dfde420f62514d0aefdc22ad
destifo <estifanosbireda@gmail.com>
2022-09-01 00:43:19 +0300

    added a solution for Quality vs Quantity --Codeforces 1646B

---
+++ B. Quality vs Quantity/QualityvsQuantity.py
@@ -0,0 +1,30 @@
+
+# O(n) time,
+# O(1) space,
+# Approach: 
+
+from typing import List
+
+
+def isPaintable(lst: List[int], n: int) -> None:
+    lst.sort()
+    l, r = 1, n-1
+    blue_sum = lst[0]
+    red_sum = 0
+    while l < r:
+        blue_sum += lst[l]
+        red_sum += lst[r]
+        l +=1
+        r -=1
+        if red_sum > blue_sum:
+            print('YES')
+            return
+
+    print('NO')
+
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    seq = list(map(int, input().split()))
+    isPaintable(seq, n)
