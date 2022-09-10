6e2ae07d6d22fbd269af8dd40bb2ddd0fbfa2a0f
destifo <estifanosbireda@gmail.com>
2022-09-10 13:55:52 +0300

    added a dp solution for Best Time to Buy and Sell Stock --leetcode 121

--- 121. Best Time to Buy and Sell Stock/MaxProfit.py
+++ 121. Best Time to Buy and Sell Stock/MaxProfit.py
@@ -2,6 +2,9 @@
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 '''
 
+from typing import List
+
+
 class Solution:
     def maxProfit(self, prices):
         n = len(prices)
@@ -14,3 +17,34 @@ class Solution:
             curr_max = max(curr_max, curr_price)
 
         return max_profit
+
+
+    # O(n) time,
+    # O(n) space,
+    # Approach: dp, memoization, recursion
+    def maxProfit2(self, prices: List[int]) -> int:
+        memo = {}
+        
+        def transaction(index: int, buying: bool) -> int:
+            
+            if index == len(prices):
+                return 0
+            
+            if (index, buying) in memo:
+                return memo[(index, buying)]
+            
+            if buying:
+                buy = transaction(index+1, not buying) - prices[index]
+                not_buy = transaction(index+1, buying)
+                
+                profit = max(buy, not_buy)
+            else:
+                sold = prices[index]
+                not_sold = transaction(index+1, buying)
+                
+                profit = max(sold, not_sold)
+                
+            memo[(index, buying)] = profit
+            return profit
+        
+        return transaction(0, buying=True)
