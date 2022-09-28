c966c3f868e6b600fb0ac7fcd773fa9f0f3202e8
destifo <estifanosbireda@gmail.com>
2022-09-28 14:56:17 +0300

    added a solution on another day for  Number of Substrings Containing All Three Characters --leetcode 1358

--- 1358. Number of Substrings Containing All Three Characters/NumberOfSubstrings.py
+++ 1358. Number of Substrings Containing All Three Characters/NumberOfSubstrings.py
@@ -39,6 +39,38 @@ class Solution:
 
         return ans
 
+
+    # O(n) time,
+    # O(1) space,
+    # Approach: two pointers, hashable
+    def numberOfSubstrings(self, s: str) -> int:
+        n = len(s)
+        window = {}
+        l, r = 0, 0
+        
+        while len(window) < 3 and r < n:
+            ch = s[r]
+            window[ch] = window.get(ch, 0) + 1
+            r +=1
+            
+        ans = 0
+        
+        while r <= n:
+            if len(window) == 3:
+                ans += (n-r+1)
+                ch = s[l]
+                window[ch] -=1
+                if window[ch] == 0:
+                    window.pop(ch)
+                l +=1
+            else:
+                if r == n: break
+                ch = s[r]
+                window[ch] = window.get(ch, 0) + 1
+                r +=1
+                
+        return ans
+
                 
 sol = Solution()
 print(sol.numberOfSubstrings("abcabc"))  
