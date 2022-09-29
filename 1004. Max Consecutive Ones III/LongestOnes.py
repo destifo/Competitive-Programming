e567b6ea68c3dd1db7bbd58730e69bb5c88b2de5
destifo <estifanosbireda@gmail.com>
2022-09-29 23:38:42 +0300

    added a solution on another day

--- 1004. Max Consecutive Ones III/LongestOnes.py
+++ 1004. Max Consecutive Ones III/LongestOnes.py
@@ -2,6 +2,9 @@
 https://leetcode.com/problems/max-consecutive-ones-iii/submissions/
 '''
 
+from typing import List
+
+
 class Solution:
     def longestOnes(self, nums, k: int):
         n = len(nums)
@@ -20,6 +23,33 @@ class Solution:
             r +=1
             
         max_len = r - l    
+        return max_len
+
+    
+    # O(n) time,
+    # O(1) space,
+    # Approach: two pointers, 
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        l, r = 0, 0
+        max_len = 0
+        
+        while r < n:
+            if nums[r] == 0:
+                if k > 0:
+                    r +=1
+                    k -=1
+                elif l == r:
+                    l +=1
+                    r +=1
+                else:
+                    max_len = max(max_len, r-l)
+                    if nums[l] == 0:    k+=1
+                    l +=1
+            else:
+                r +=1
+        
+        max_len = max(max_len, r-l)
         return max_len
             
 sol = Solution()
