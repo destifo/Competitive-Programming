8261ec241597ea2f8eb66296c8439a254d4d39ab
destifo <estifanosbireda@gmail.com>
2022-09-29 22:26:16 +0300

    added a constant space solution for Maximum Sum of Two Non-Overlapping Subarrays --leetcode 1031

--- 1031. Maximum Sum of Two Non-Overlapping Subarrays/MaxSumTwoNoOverlap.py
+++ 1031. Maximum Sum of Two Non-Overlapping Subarrays/MaxSumTwoNoOverlap.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/
 '''
 
 import heapq
+from typing import List
 
 
 class Solution:
@@ -88,6 +89,35 @@ class Solution:
                     heapq.heappush(maxheap, -1 * (val1[2] + val2[2]))
 
         return -1 * maxheap[0]
+
+    
+    # O(n^2) time,
+    # O(1) space,
+    # Approach: prefix sum
+    def maxSumTwoNoOverlap3(self, nums: List[int], firstLen: int, secondLen: int) -> int:
+        n = len(nums)
+        max_sum = 0
+        
+        for i in range(1, n):
+            nums[i] += nums[i-1]
+            
+        def findSubarraySum(l: int, r: int) -> int:
+            if l == 0:
+                return nums[r]
+            
+            return nums[r] - nums[l-1]
+        
+        for i in range(0, n-firstLen+1):
+            first_tot = findSubarraySum(i, i+firstLen-1)
+            for j in range(0, i-secondLen+1):
+                second_tot = findSubarraySum(j, j+secondLen-1)
+                max_sum = max(max_sum, second_tot + first_tot)
+                
+            for j in range(i+firstLen, n-secondLen+1):
+                second_tot = findSubarraySum(j, j+secondLen-1)
+                max_sum = max(max_sum, second_tot + first_tot)
+                
+        return max_sum
 
 
 sol = Solution()
