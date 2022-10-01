7ce5abc520647ee36c26f1331f3281051c618ab5
destifo <estifanosbireda@gmail.com>
2022-10-01 13:32:19 +0300

    added two solutions of another day for Decode Ways --leetcode 91

--- 91. Decode Ways/NumDecodings.py
+++ 91. Decode Ways/NumDecodings.py
@@ -28,6 +28,53 @@ class Solution:
         
         return dp[0]
 
+    
+    # O(n) time,
+    # O(n) space,
+    # Approach: dp top-down, memoization
+    def numDecodings2(self, s: str) -> int:
+        n = len(s)
+        memo = {}
+        
+        def findDecodings(index: int) -> int:
+            if s[index] == '0':
+                return 0
+            
+            if index in memo:
+                return memo[index]
+            
+            if index == n-1:
+                return 1
+            
+            if index == n-2:
+                memo[index] = (1 if int(s[index:]) < 27 else 0) + findDecodings(index+1)
+                return memo[index]
+            
+            memo[index] = findDecodings(index+1) + (findDecodings(index+2) if int(s[index:index+2]) < 27 else 0)
+            return memo[index]
+        
+        return findDecodings(0)
+
+    
+    # O(n) time,
+    # O(n) space,
+    # Approach: dp bottom-up, tabulation
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        if len(s) == 1:
+            return 1 if s[0] != '0' else 0
+        
+        table = [0 for i in range(n)]
+        table[n-1] = 1 if s[n-1] != '0' else 0
+        table[n-2] = (table[n-1] + (1 if int(s[n-2:]) < 27 else 0)) if s[n-2] != '0' else 0
+        
+        for i in range(n-3, -1, -1):
+            one_jump = 0 if s[i] == '0' else table[i+1]
+            two_jump = table[i+2] if int(s[i:i+2]) < 27 and s[i] != '0' else 0
+            table[i] = one_jump + two_jump
+            
+        return table[0]
+
 
 sol = Solution()
 print(sol.numDecodings("12"))
