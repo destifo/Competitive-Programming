b1ec933ce70c479d66bc3055d9efe5a8ca052035
destifo <estifanosbireda@gmail.com>
2022-11-19 13:26:50 +0300

    added a rust solution for Valid Perfect Square --leetcode 367

---
+++ 367. Valid Perfect Square/is_perfect_square.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    
+    // O(logn) time,
+    // O(1) space,
+    // Approach: binary search, math
+    pub fn is_perfect_square(num: i32) -> bool {
+        let mut low = 1;
+        let mut hi = (46341);
+        
+        while low <= hi {
+            let mid = low + (hi-low)/2;
+            let square = (mid*mid);
+            
+            if square == num {
+                return true;
+            }
+            else if square < num{
+                low = mid+1;
+            }
+            else {
+                hi = mid-1;
+            }
+        }
+        
+        return false;
+    }
+}
