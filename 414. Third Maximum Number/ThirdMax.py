028721d4061ef059117b6f096bce5a907037e483
destifo <estifanosbireda@gmail.com>
2023-02-08 12:02:39 +0300

    added a one pass solution for Third Maximum Number --leetcode 414

--- 414. Third Maximum Number/ThirdMax.py
+++ 414. Third Maximum Number/ThirdMax.py
@@ -3,6 +3,9 @@ https://leetcode.com/problems/third-maximum-number/
 '''
 
 
+from typing import List
+
+
 class Solution:
     def thirdMax(self, nums: list[int]) -> int:
         n = len(nums)
@@ -18,3 +21,25 @@ class Solution:
             vstd.add(num)
             
         return nums[-1]
+
+
+    # O(n) time,
+    # O(1) space,
+    # Approach: array, 
+    def thirdMax2(self, nums: List[int]) -> int:
+        first_max = float('-inf')
+        second_max = float('-inf')
+        third_max = float('-inf')
+        
+        for num in nums:
+            if num > first_max:
+                third_max = second_max
+                second_max = first_max
+                first_max = num
+            elif num > second_max and num != first_max:
+                third_max = second_max
+                second_max = num
+            elif num > third_max and num != second_max and num != first_max:
+                third_max = num
+                
+        return third_max if third_max != float('-inf') else first_max
