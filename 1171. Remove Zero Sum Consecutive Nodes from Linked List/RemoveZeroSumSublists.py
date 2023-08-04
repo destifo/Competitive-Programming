9f2a1afe0b0c7e1fff5540d31384d03bbd64290f
destifo <estifanosbireda@gmail.com>
2023-08-04 11:04:54 +0300

    added a more efficient solution for Remove Zero Sum Consecutive Nodes from Linked List --leetcode 1171

--- 1171. Remove Zero Sum Consecutive Nodes from Linked List/RemoveZeroSumSublists.py
+++ 1171. Remove Zero Sum Consecutive Nodes from Linked List/RemoveZeroSumSublists.py
@@ -8,7 +8,7 @@ class ListNode:
         self.next = next
 class Solution:
     
-    # O(n) time,
+    # O(n^2) time,
     # O(n) space,
     # Approach: stack, prefix sum, linked list, 
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -30,6 +30,33 @@ class Solution:
                 stack.pop()
                 prefix_sum.pop()
                 
+            stack[-1].next = curr.next
+            curr = curr.next
+                
+        return dummy.next
+    
+    
+    # O(n) time,
+    # O(n) space,
+    # Approach: stack, prefix sum, hash map, linked list
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(1001, head)
+        stack = [dummy]
+        prefix_sum = [0]
+        visited = {0: 1}
+        
+        curr = head
+        while curr:
+            prefix_sum.append(prefix_sum[-1]+curr.val)
+            length = visited.get(prefix_sum[-1], len(prefix_sum))
+            stack.append(curr)
+            visited[prefix_sum[-1]] = len(prefix_sum)
+            while len(stack) > length:
+                stack.pop()
+                if prefix_sum[-1] in visited:
+                    visited.pop(prefix_sum[-1])
+                prefix_sum.pop()
+            visited[prefix_sum[-1]] = len(prefix_sum)  
             stack[-1].next = curr.next
             curr = curr.next
                 
