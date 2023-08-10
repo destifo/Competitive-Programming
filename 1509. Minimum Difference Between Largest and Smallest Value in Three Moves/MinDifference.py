12f64095378a0381648891d794fdfcd133e4bc96
destifo <estifanosbireda@gmail.com>
2023-08-10 20:53:51 +0300

    added a solution for Minimum Difference Between Largest and Smallest Value in Three Moves --leetcode 1509

---
+++ 1509. Minimum Difference Between Largest and Smallest Value in Three Moves/MinDifference.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    
+    def findMinDiff(self, left: int, right: int, rem: int, nums: List[int]) -> int:
+        
+        if left == right:
+            return 0
+        
+        if rem == 0:
+            return nums[right]-nums[left]
+        
+        remove_left = self.findMinDiff(left+1, right, rem-1, nums)
+        remove_right = self.findMinDiff(left, right-1, rem-1, nums)
+        
+        return min(remove_left, remove_right)
+    
+    
+    # O(nlogn) time,
+    # O(1) space,
+    # Approach: recursion, decison tree
+    def minDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return self.findMinDiff(0, len(nums)-1, 3, nums)
