2728736eca11d6eb2bdb8566347a7dbdaa59d25f
destifo <estifanosbireda@gmail.com>
2023-08-17 16:32:16 +0300

    added another solution for 01 Matrix --leetcode 542

--- 542. 01 Matrix/UpdateMatrix.py
+++ 542. 01 Matrix/UpdateMatrix.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import List, Tuple
 
 
 class Solution:
@@ -50,6 +51,58 @@ class Solution:
         bfs()
         
         return result
+    
+    
+    def inBound(self, row: int, col: int, mat: List[List[int]]) -> bool:
+        
+        if row < 0 or row >= len(mat):
+            return False
+        
+        if col < 0 or col >= len(mat[0]):
+            return False
+        
+        return True
+    
+    
+    def getNeighbors(self, row: int, col: int, mat: List[List[int]]) -> List[Tuple[int]]:
+        nbrs = []
+        
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for x, y in directions:
+            new_row, new_col = x+row, y+col
+            if self.inBound(new_row, new_col, mat) and mat[new_row][new_col] == 1:
+                nbrs.append((new_row, new_col))
+                
+        return nbrs
+    
+    
+    # O(n*m) time,
+    # O(n*m) space,
+    # Approach: bfs, graph, 
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        queue = deque()
+        n, m = len(mat), len(mat[0])
+        
+        for row in range(n):
+            for col in range(m):
+                if mat[row][col] == 0:
+                    queue.append((row, col))
+                    
+        ans = [[0 for _ in range(m)] for _ in range(n)]
+        dist = 0
+        while queue:
+            queue_len = len(queue)
+            for _ in range(queue_len):
+                row, col = queue.popleft()
+                nbrs = self.getNeighbors(row, col, mat)
+                ans[row][col] = dist
+                for nbr in nbrs:
+                    mat[nbr[0]][nbr[1]] = 0
+                    queue.append(nbr)
+                
+            dist += 1
+            
+        return ans
             
 
 sol = Solution()
