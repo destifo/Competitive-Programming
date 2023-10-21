b5189073e570579353c605b0912e4ee9c7aa97c5
destifo <estifanosbireda@gmail.com>
2023-10-21 21:55:13 +0300

    added another solution for Maximum Subarray --leetcode 53

--- 53. Maximum Subarray/MaxSubArray.py
+++ 53. Maximum Subarray/MaxSubArray.py
@@ -23,3 +23,20 @@ class Solution:
         temp = self.findMaxSum(0, nums)
         
         return max(self.max_sum, temp)
+    
+    
+    # O(n) time,
+    # O(1) space,
+    # Approach: prefix sum, 
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans = float('-inf')
+        prev_smallest = 0
+        tot = 0
+        
+        for num in nums:
+            tot += num
+            curr_best_sum = (tot-prev_smallest)
+            ans = max(ans, curr_best_sum)
+            prev_smallest = min(prev_smallest, tot)
+            
+        return ans
