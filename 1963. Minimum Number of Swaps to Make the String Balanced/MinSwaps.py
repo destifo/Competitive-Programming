c08e0904fc9136172baf8311bd3ac59a7f0590fc
destifo <estifanosbireda@gmail.com>
2023-11-25 13:37:04 +0300

    added a one pass solution for Minimum Number of Swaps to Make the String Balanced --leetcode 1963

--- 1963. Minimum Number of Swaps to Make the String Balanced/MinSwaps.py
+++ 1963. Minimum Number of Swaps to Make the String Balanced/MinSwaps.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Solution:
     
     # O(n) time,
@@ -26,3 +29,20 @@ class Solution:
                 diff += 2
                 
         return swaps
+    
+    
+    # O(n) time,
+    # O(1) space,
+    # Approach: counting, greedy, 
+    def minSwaps2(self, s: str) -> int:
+        extra_closed = 0
+        
+        max_closed = 0
+        for ch in s:
+            if ch == ']':
+                extra_closed += 1
+            else:
+                extra_closed -= 1   
+            max_closed = max(max_closed, extra_closed)
+            
+        return math.ceil(max_closed/2)
