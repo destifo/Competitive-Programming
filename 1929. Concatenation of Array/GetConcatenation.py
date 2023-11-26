31cdf3d2670f2b64b0b86dc43090d526fc2ea232
destifo <estifanosbireda@gmail.com>
2023-11-26 13:52:41 +0300

    added a solution for Concatenation of Array --leetcode 1929

---
+++ 1929. Concatenation of Array/GetConcatenation.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    
+    # O(n) time,
+    # O(n) space,
+    # Approach: array
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
