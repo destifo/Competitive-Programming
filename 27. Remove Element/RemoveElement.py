4b09ff46add3f5a2141ea471321e13c60358e5e7
destifo <estifanosbireda@gmail.com>
2023-11-28 01:22:15 +0300

    added different day solution for Remove Element --leetcode 27

--- 27. Remove Element/RemoveElement.py
+++ 27. Remove Element/RemoveElement.py
@@ -2,6 +2,9 @@
 https://leetcode.com/problems/remove-element/
 '''
 
+from typing import List
+
+
 class Solution:
     def removeElement(self, nums, val: int):
         n = len(nums)
@@ -24,3 +27,27 @@ class Solution:
                 l +=1
                 
         return r if nums[r] == val else r + 1
+    
+    
+    def getLast(self, val: int, index: int, nums: List[int]) -> int:
+        while index >= 0 and nums[index] == val:
+            index -= 1
+            
+        return index
+    
+    
+    # O(n) time,
+    # O(1) space,
+    # Approach: two pointers, 
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        last = self.getLast(val, n-1, nums)
+        
+        i = 0
+        while i < last:
+            if nums[i] == val:
+                nums[i], nums[last] = nums[last], nums[i]
+                last = self.getLast(val, last-1, nums)
+            i += 1
+            
+        return last+1
