c50dc7edd1559017105430f3ae258b69531790ed
destifo <estifanosbireda@gmail.com>
2023-12-07 20:24:17 +0300

    added another day solution for Word Search --leetcode 79

--- 79. Word Search/Exist.py
+++ 79. Word Search/Exist.py
@@ -59,6 +59,59 @@ class Solution:
                     vstd = set()
                     
         return False
+    
+    
+    def inBounds(self, row: int, col: int, board: List[List[str]]) -> bool:
+        if row < 0 or row >= len(board):
+            return False
+        
+        if col < 0 or col >= len(board[0]):
+            return False
+        
+        return True
+    
+    
+    def getNeighbors(self, row: int, col: int, board: List[List[str]]) -> List[Tuple[int]]:
+        
+        nbrs = []
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        for x, y in directions:
+            new_row, new_col = row+x, col+y
+            if self.inBounds(new_row, new_col, board) and board[new_row][new_col] != 0:
+                nbrs.append((new_row, new_col))
+                
+        return nbrs
+    
+    
+    def findWord(self, row: int, col: int, index: int, word: str, board: List[List[str]]) -> bool:
+        if board[row][col] != word[index]:
+            return False
+        index +=1 
+        if index == len(word):
+            return True
+        
+        board[row][col] = 0
+        for nbr_row, nbr_col in self.getNeighbors(row, col, board):
+            if self.findWord(nbr_row, nbr_col, index, word, board):
+                return True
+            
+        board[row][col] = word[index-1]
+        return False
+    
+    
+    # O((n*m)^2) time,
+    # O((n*m)^2) space,
+    # Approach: dfs, backtracking,  
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == word[0] and self.findWord(row, col, 0, word, board):
+                    return True
+                    
+        return False
 
 sol = Solution()
 print(sol.exist([["C","A","A"],["A","A","A"],["B","C","D"]]
