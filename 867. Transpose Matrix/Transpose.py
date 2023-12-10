073c2fd7cdbb0f90ebbe5f3a23a5f1e105f09a19
destifo <estifanosbireda@gmail.com>
2023-12-10 13:02:26 +0300

    added another day solution for Transpose Matrix --leetcode 867

--- 867. Transpose Matrix/Transpose.py
+++ 867. Transpose Matrix/Transpose.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def transpose(self, matrix):
         m = len(matrix)
@@ -10,6 +13,31 @@ class Solution:
                 result[j][i] = num
                 
         return result
+    
+    
+    def rotate(self, matrix: List[List[int]]) -> None:
+        
+        for row in range(len(matrix)):
+            for col in range(row+1, len(matrix)):
+                matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+    
+    
+    # O(n*m) time,
+    # O(n*m) space,
+    # Approach: matrix, 
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        
+        if ROWS == COLS:
+            self.rotate(matrix)
+            return matrix
+        
+        ans = [[0 for _ in range(ROWS)] for _ in range(COLS)]
+        for row in range(ROWS):
+            for col in range(COLS):
+                ans[col][row] = matrix[row][col]
+            
+        return ans
 
 
 sol = Solution()
