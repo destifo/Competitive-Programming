ae080f4b857ef503d87707350de71acc9af90e9a
destifo <estifanosbireda@gmail.com>
2023-12-26 09:23:15 +0300

    added another day solution for Number of Dice Rolls With Target Sum --leetcode 1155

--- 1155. Number of Dice Rolls With Target Sum/NumRollsToTarget.py
+++ 1155. Number of Dice Rolls With Target Sum/NumRollsToTarget.py
@@ -1,3 +1,6 @@
+from typing import Dict, Tuple
+
+
 class Solution:
     # O(n*target) time,
     # O(n*target) space,
@@ -25,3 +28,32 @@ class Solution:
             return memo[(depth, rem)]
         
         return findCombinations(0, target)
+    
+    
+    def findRolls(self, n: int, k: int, rem: int, memo: Dict[Tuple[int], int]) -> int:
+        
+        if rem == 0 and n == 0:
+            return 1
+        
+        if n == 0 or rem <= 0:
+            return 0
+        
+        state = (n, rem)
+        if state in memo:
+            return memo[state]
+        
+        ways = 0
+        for i in range(1, k+1):
+            ways += self.findRolls(n-1, k, rem-i, memo)
+        
+        memo[state] = ways
+        return ways
+    
+    
+    # O(n*target*k) time,
+    # O(n*target*k) space,
+    # Approach: dp, top down
+    def numRollsToTarget2(self, n: int, k: int, target: int) -> int:
+        
+        MOD = 10**9 + 7
+        return self.findRolls(n, k, target, {}) % MOD
