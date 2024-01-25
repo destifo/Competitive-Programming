b382d8c9b600ee0b9256e9c6eef475576c49e76b
destifo <estifanosbireda@gmail.com>
2024-01-25 13:56:21 +0300

    add a solution for Longest Common Subsequence --leetcode 1143

--- 1143. Longest Common Subsequence/LongestCommonSubsequence.py
+++ 1143. Longest Common Subsequence/LongestCommonSubsequence.py
@@ -50,3 +50,20 @@ class Solution:
                     table[i][j] = max(table[i-1][j], table[i][j-1])
                     
         return table[n][m]
+    
+    
+    # O(n*m) time,
+    # O(n*m) space,
+    # Approach: bottom up dp, 
+    def longestCommonSubsequence2(self, text1: str, text2: str) -> int:
+        n, m = len(text1), len(text2)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+                
+        return dp[0][0]
