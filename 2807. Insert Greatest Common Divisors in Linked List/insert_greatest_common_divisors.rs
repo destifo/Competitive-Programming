255ad1ff160356f43988b1110bb8da7d2390bfb2
destifo <estifanosbireda@gmail.com>
2024-02-12 00:01:47 +0300

    refactor: just formatting

--- 2807. Insert Greatest Common Divisors in Linked List/insert_greatest_common_divisors.rs
+++ 2807. Insert Greatest Common Divisors in Linked List/insert_greatest_common_divisors.rs
@@ -4,7 +4,7 @@
 //   pub val: i32,
 //   pub next: Option<Box<ListNode>>
 // }
-// 
+//
 // impl ListNode {
 //   #[inline]
 //   fn new(val: i32) -> Self {
@@ -18,35 +18,35 @@
 fn get_gcd(num1: i32, num2: i32) -> i32 {
     match num2 {
         0 => num1,
-        _ => get_gcd(num2, num1 % num2)
+        _ => get_gcd(num2, num1 % num2),
     }
 }
 
 impl Solution {
-    pub fn insert_greatest_common_divisors(mut head:  Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-      
-        
+    pub fn insert_greatest_common_divisors(
+        mut head: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
         /*
             1) store curr, and prev
             2) create gcd node, and add in the middle,
             3) update prev to curr, curr = curr.next
         */
-        
+
         let mut curr = &mut head;
-        
+
         while let Some(node) = curr {
             if let Some(mut next) = node.next.take() {
                 let gcd = get_gcd(next.val, node.val);
                 let mut gcd_node = Box::new(ListNode::new(gcd));
                 gcd_node.next = Some(next);
-                
+
                 node.next = Some(gcd_node);
                 curr = &mut node.next.as_mut().unwrap().next;
             } else {
-                break
+                break;
             }
         }
-        
+
         return head;
     }
 }
