eeb84effcf8262c68c4214efa2fe8c5c37825f14
destifo <estifanosbireda@gmail.com>
2024-03-04 10:59:33 +0300

    add a different day solution for Bag of Tokens --leetcode 948

--- 948. Bag of Tokens/BagOfTokensScore.py
+++ 948. Bag of Tokens/BagOfTokensScore.py
@@ -32,6 +32,35 @@ class Solution:
             max_score = max(max_score, curr_score)
             
         return max_score
+    
+    
+    # O(nlogn) time,
+    # O(1) space,
+    # Approach: greedy, sorting, two pointers
+    def bagOfTokensScore2(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        
+        left, right = 0, len(tokens)-1
+        max_score = 0
+        score = 0
+        
+        
+        while left <= right:
+            if score > 0 and power < tokens[left]:
+                power += tokens[right]
+                right -= 1
+                score -= 1
+            
+            if power >= tokens[left]:
+                score += 1
+                power -= tokens[left]
+                left += 1
+            else:
+                break
+                
+            max_score = max(max_score, score)
+        
+        return max_score
 
 
 sol = Solution()
