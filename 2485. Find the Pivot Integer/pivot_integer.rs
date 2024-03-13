7a7ed65e8114264ffcf1d78969a825d2053b1421
destifo <estifanosbireda@gmail.com>
2024-03-14 00:45:09 +0300

    add a solution for Find the Pivot Integer --leetcode 2485

---
+++ 2485. Find the Pivot Integer/pivot_integer.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    
+    // O(n) time, one pass
+    // O(1) space,
+    // Approach: prefix sum, 
+    pub fn pivot_integer(n: i32) -> i32 {
+        let summation = (n*(n+1))/2;
+        
+        let mut curr_tot = 0;
+        for i in 1..=n {
+            let right = summation-curr_tot;
+            curr_tot += i;
+            let left = curr_tot;
+            if left == right {
+                return i;
+            }
+        }
+        
+        -1
+    }
+}
