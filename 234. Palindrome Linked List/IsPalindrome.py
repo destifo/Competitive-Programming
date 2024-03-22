cc60c93de8d2bafcf4b83a95bb8b76350ff70c5a
destifo <estifanosbireda@gmail.com>
2024-03-22 10:40:12 +0300

    add another day solution for Palindrome Linked List --leetcode 234

--- 234. Palindrome Linked List/IsPalindrome.py
+++ 234. Palindrome Linked List/IsPalindrome.py
@@ -1,5 +1,5 @@
 '''
-
+https://leetcode.com/problems/palindrome-linked-list/
 '''
 
 
@@ -19,7 +19,7 @@ class Solution:
         slow = head
         fast = head
         
-        while fast != None and fast.next != None:
+        while fast is not None and fast.next is not None:
             slow = slow.next
             fast = fast.next.next
             
@@ -37,4 +37,41 @@ class Solution:
             curr = curr.next
             prev = prev.next
         
+        return True
+    
+    
+    # O(n) time,
+    # O(1) space,
+    # Approach: linked list, two pointers, hair-tortoise
+    def isPalindrome2(self, head: Optional[ListNode]) -> bool:
+        if not head.next:
+            return True
+        
+        slow, fast = head, head
+        
+        # find middle node
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse nodes starting from middle
+        prev.next = None
+        prev, curr = slow, slow.next
+        prev.next = None
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            
+        # check pali
+        left, right = head, prev
+        while left and right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+            
         return True
