d45bb3a77884a8e4273d2c4436590f677a1e0491
destifo <estifanosbireda@gmail.com>
2024-04-15 14:47:20 +0300

    add another day solution for Sum Root to Leaf Numbers --leetcode 129

--- 129. Sum Root to Leaf Numbers/SumNumbers.py
+++ 129. Sum Root to Leaf Numbers/SumNumbers.py
@@ -36,3 +36,35 @@ class Solution:
                 
         dfs(root, [str(root.val)])
         return ans[0]
+    
+    
+    # O(n) time, n -> no of nodes,
+    # O(n) space,
+    # Approach: dfs, 
+    def sumNumbers2(self, root: Optional[TreeNode]) -> int:
+        
+        ans = 0
+        
+        def is_leaf(node: Optional[TreeNode]) -> bool:
+            return not node.left and not node.right
+        
+        
+        def search_numbers(root: Optional[TreeNode], digits: List[int]) -> None:
+            nonlocal ans
+            if is_leaf(root):
+                digits.append(str(root.val))
+                num = int("".join(digits))
+                ans += num
+                digits.pop()
+                return
+            
+            digits.append(str(root.val))
+            if root.left:
+                search_numbers(root.left, digits)
+            if root.right:
+                search_numbers(root.right, digits)
+            digits.pop()
+        
+        if root:
+            search_numbers(root, [])
+        return ans
