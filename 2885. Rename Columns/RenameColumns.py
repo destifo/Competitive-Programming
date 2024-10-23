a98da99e79fb6171b8661778fe759ad36dedf48f
destifo <estifanosbireda@gmail.com>
2024-10-23 11:56:29 +0300

    feat(2885. Rename Columns): add a python solution --leetcode

---
+++ 2885. Rename Columns/RenameColumns.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    df = students.rename(columns={
+        "id": "student_id",
+        "first": "first_name",
+        "last": "last_name",
+        "age": "age_in_years"
+    })
+    
+    return df
